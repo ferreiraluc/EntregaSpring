b96750110209d6e28e9af4c6457550d05294b5cd
lucas.gimenez <l.adrianobf@gmail.com>
2023-08-08 20:26:21 -0300

    create remote files

---
+++ src/test/java/com/entregaSpring/EntregaSpring/EntregaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entregaSpring.EntregaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
